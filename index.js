93b27b9c6993afadfa4758caea7f0367cc01b2d2
ZeppieHub <59137982+ZeppieHub@users.noreply.github.com>
2020-10-13 15:26:36 +0300

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,51 @@
+ezdb = module.exports;
+const fs = require('fs');
+
+ezdb.set = (dbName, dbValue) => {
+    if(!dbName) return console.error("Write a database name!");
+    if(!dbValue) return console.error("Write a value for database!");
+    var print = { "name":dbName, "value":dbValue };
+    var data = require('./data.json');
+    data[dbName] = print;
+    fs.writeFileSync('./node_modules/dc-ez.db/data.json', JSON.stringify(data));
+    return true;
+}
+
+ezdb.get = dbName => {
+    if(!dbName) return console.error("Write a database name!");
+    var data = require('./data.json');
+    var print = data[dbName];
+    if(!print) return console.error(`"${dbName}" cannot find!`);
+    return print["value"];
+}
+
+ezdb.add = (dbName, dbValue) => {
+    if(!dbName) return console.error("Write a database name!");
+    if(!dbValue) return console.error("Write a value for database!");
+    var data = require('./data.json');
+    data[dbName]["value"] = data[dbName]["value"] + dbValue;
+    fs.writeFileSync('./node_modules/dc-ez.db/data.json', JSON.stringify(data));
+    return true;
+}
+
+ezdb.remove = dbName => {
+    if(!dbName) return console.error("Write a database name!");
+    var data = require('./data.json');
+    data[dbName] = {  };
+    fs.writeFileSync('./node_modules/dc-ez.db/data.json', JSON.stringify(data));
+    return true;
+}
+
+ezdb.push = (dbName, dbValue) => {
+    if(!dbName) return console.error("Write a database name!");
+    if(!dbValue) return console.error("Write a value for database!");
+    var data = require('./data.json');
+    if(typeof data[dbName]["value"] !== 'object') return console.error("This database's value is not a array/list!");
+    try{
+        data[dbName]["value"].push(dbValue);
+        fs.writeFileSync('./node_modules/dc-ez.db/data.json', JSON.stringify(data));
+    } catch (err) {
+        return console.error(err);
+    }
+    return true;
+}
